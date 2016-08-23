23c3e01da610b5808c839fa6c82de36fdfe1edcd
Yuleson <yuleson@foxmail.com>
2016-08-23 10:34:56 +0800

    updat8,23e

---
+++ src/pro11.py
@@ -0,0 +1,15 @@
+'''
+Created on 2016年8月18日
+
+@author: yuanyun.yy
+'''
+tuple1 = (1,2,3,4,5,6,7,8)
+print(tuple1[5:7])
+tuple2 = (1,)
+tuple3 = (1)
+tuple4 = 1,
+print(type(tuple2))
+print(type(tuple3))
+print(type(tuple4))
+print(8 * (8))
+print(8 * (8,))
